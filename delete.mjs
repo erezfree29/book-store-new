21ebd326477340e6fac3c35822be1207dac56b36
Erez Friemagor <erezfree29@gmail.com>
2021-09-04 06:39:56 +0100

    delete function

---
+++ delete.mjs
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7QlpK6CmgdU0hZt33zXJ/books/';
+const id = 'kt2wk8ey';
+const url = new URL(`${baseURL}${id}`);
+const data = {
+  item_id: id,
+};
+
+fetch(url, {
+
+  method: 'DELETE',
+
+  // Adding body or contents to send
+  body: JSON.stringify(data),
+
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
