443763417c34e74317c85c63ca3ff70470f4d043
Erez Friemagor <erezfree29@gmail.com>
2021-09-07 21:18:58 +0100

    add book component

---
+++ src/components/book.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
+/* eslint-disable arrow-body-style */
+const Book = () => {
+  return (
+    <div>
+      <h3>Book Display</h3>
+    </div>
+  );
+};
+
+export default Book;
